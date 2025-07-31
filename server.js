fb126b691b53aa3e3d830c04f2853509d1d9e800
balakrishna6023 <128021148+balakrishna6023@users.noreply.github.com>
2025-07-31 08:35:45 +0530

    second

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 const app = express();
 
 app.use(cors());
-
-app.get('/api/message', (req, res) => {
-  res.json({ message: 'Hello from backend!' });
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      frameAncestors: ["'none'"],
+    },
+  })
+);
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from backend!" });
 });
 
 app.listen(3000, () => {
-  console.log('Backend running on http://localhost:3000');
+  console.log("Backend running on http://localhost:3000");
 });
