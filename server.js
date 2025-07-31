ace2e6ac9a96a3734acc806a6374c1a6aceb1d5c
balakrishna6023 <128021148+balakrishna6023@users.noreply.github.com>
2025-07-31 09:52:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const helmet = require("helmet");
+//const helmet = require("helmet");
 const app = express();
 
 app.use(cors());
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      frameAncestors: ["'none'"],
-    },
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "frame-ancestors 'none'; default-src 'self';");
+  res.setHeader("X-Frame-Options", "DENY");
+  next();
+});
 app.get("/api/message", (req, res) => {
   res.json({ message: "Hello from backend!" });
 });
