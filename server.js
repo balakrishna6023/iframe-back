bf3aa71e78762fba5b83b75db38334ac6bfa6aa0
balakrishna6023 <128021148+balakrishna6023@users.noreply.github.com>
2025-07-31 08:33:34 +0530

    first

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const app = express();
+
+app.use(cors());
+
+app.get('/api/message', (req, res) => {
+  res.json({ message: 'Hello from backend!' });
+});
+
+app.listen(3000, () => {
+  console.log('Backend running on http://localhost:3000');
+});
